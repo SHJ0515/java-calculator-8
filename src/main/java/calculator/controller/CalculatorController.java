0ea79961482927355d6a4fce5453b068c2b932d1
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-10-20 14:06:17 +0900

    feat(controller): CalculatorController 구현 - view와 service 연동 - try catch로 IllegalArugumentException 처리

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,29 @@
+package calculator.controller;
+
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CalculatorService calculatorService;
+
+    public CalculatorController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.calculatorService = new CalculatorService();
+    }
+
+    public void run() {
+        try {
+            String input = inputView.input();
+            int result = calculatorService.calculate(input);
+            outputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            throw e;
+        }
+    }
+}
